0d20d14ab2a72d275565b7ba1c70f631ac6f6cfb
Carlos Quintero <cquintero@bp.fi.cr>
2022-12-15 09:34:50 -0600

    Add Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/Layout/header/header.component';
+import { FooterComponent } from './components/Layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
