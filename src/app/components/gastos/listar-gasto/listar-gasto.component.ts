b0dd8247d76776daedf8acb8e6c8e3992eed11e1
clariisgr <clariisgr@gmail.com>
2022-12-20 20:35:38 -0600

    Editar gasto - Clarissa

--- src/app/components/gastos/listar-gasto/listar-gasto.component.ts
+++ src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -1,34 +1,64 @@
-import { Component, Input, } from "@angular/core";
+import { Component, Input, OnInit, } from "@angular/core";
 import { Gasto,GastoDetalle } from '../../../shared/Gasto';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {CategoriaService} from '../../../services/categoria.service'
+import { Categoria } from '../../../shared/Categoria';
 
 @Component({
   selector: 'app-listar-gasto',
   templateUrl: './listar-gasto.component.html',
   styleUrls: ['./listar-gasto.component.css']
 })
-export class ListarGastoComponent {
+export class ListarGastoComponent implements OnInit{
   @Input() presupuesto!: Gasto|undefined;
   @Input() gastodetalle!: GastoDetalle;
+  @Input() gastoNombreEditar!: string;
+  @Input() gastoCategoriaEditar!: string;
+  @Input() gastoMontoEditar!: string;
 
   gastoDetailDelete!:GastoDetalle;
   gastoIndexDetailDelete!:number;
+
+  gastoDetailEditar!:GastoDetalle;
+  gastoIndexDetailEditar!:number;
+
+  gastoDetailMontoAntiguo!:number;
+
+  categorias!: Categoria[] | undefined;
+
   monto!:number;
   nombre!:string;
   categoria!:string;
 
-  constructor(private modalService: NgbModal) {
+  constructor(private modalService: NgbModal, private categoriaService: CategoriaService) {
+  }
+  ngOnInit(): void {
+    this.categorias = this.categoriaService.get_categorias();
   }
 
   eliminarGasto(){
     this.presupuesto?.gastodetalle?.splice(this.gastoIndexDetailDelete,1)
     this.presupuesto!.balance=this.presupuesto!.balance + this.gastoDetailDelete.monto;
-    this.presupuesto!.gastototal=this.presupuesto!.gastototal - this.gastoDetailDelete.monto;    
+    this.presupuesto!.gastototal=this.presupuesto!.gastototal - this.gastoDetailDelete.monto;
   }
 
+  editarGasto(){
+    console.log("gastoDetailMontoAntiguo", this.gastoDetailMontoAntiguo);
+    console.log("this.gastoDetailEditar.monto",  this.gastoDetailEditar.monto)
+    console.log("balance",  this.presupuesto!.balance)
+    this.presupuesto!.gastodetalle![this.gastoIndexDetailEditar] = this.gastoDetailEditar;
+    this.presupuesto!.balance=this.presupuesto!.balance + this.gastoDetailMontoAntiguo - this.gastoDetailEditar.monto;
+    this.presupuesto!.gastototal=this.presupuesto!.gastototal - this.gastoDetailMontoAntiguo + this.gastoDetailEditar.monto;
+  }
   public open(modal: any,gastoDelete:GastoDetalle,index:number): void {
     this.gastoDetailDelete=gastoDelete;
     this.gastoIndexDetailDelete=index;
+    this.modalService.open(modal);
+  }
+
+  public openEdicion(modal: any,gastoEditar:GastoDetalle,index:number): void {
+    this.gastoDetailEditar=gastoEditar;
+    this.gastoIndexDetailEditar=index;
     this.modalService.open(modal);
   }
 
